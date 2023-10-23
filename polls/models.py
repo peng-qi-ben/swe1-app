196725cdb298c34b58dc2a6819afaa08bbfb1d6c
Qi Peng <qp2039@nyu.edu>
2023-10-23 01:49:55 -0400

    increase coverage

--- polls/models.py
+++ polls/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
-# Create your models here.
-
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
